2603e1f3ed93f8ce431af467b35d4dcf0fd3615d
Ashwani Kumar Kushwaha <abic.ashwani@gmail.com>
2023-05-25 07:10:22 +0530

    spelling correction

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,74 +1,81 @@
-const User=require('../models/auth.model')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
-const key=require('../config/secretkey.config')
-const Chat=require('../models/chat.model')
-exports.signup= async(req,res)=>{
-    console.log("req body",req.body)
-    const userObj={
-        name:req.body.name,
-        email:req.body.email,
-        phone:req.body.phone,
-        password:bcrypt.hashSync(req.body.password,8),
-        country:req.body.country,
-        image:req.file.filename
-    }
-    try{
-        const user= await User.create(userObj)
-        res.render('pages-login')
-    }
-    catch(error){
-        console.log(error.message)
-        res.status(404).send({msg:"Something went wrong while creating new user"})
-    }
-}
-exports.signin=async(req,res)=>{
-    try{
-        let user=await User.findOne({email:req.body.email})
-        let otheruser=await User.find({email:{$nin:[user.email]}})
-        if(!user){
-            return res.status(201).send({
-                msg:"User does not exist"
-            })
-        }
-        const isValidPassword=bcrypt.compareSync(req.body.password,user.password)
-        if(!isValidPassword){
-            return res.status(201).send({msg:"Password does not match"})
-        }
-        const token=jwt.sign({_id:user},process.env.SECRET_KEY)
-        const response={
-            name:req.body.name,
-            email:req.body.email,
-            phone:req.body.phone,
-            location:req.body.location,
-            token:token
-        }
-        res.render('chat',{user:user,other:otheruser})
-    }catch(error){
-        console.log(error.message)
-        res.status(404).send({
-            msg:"Something went wrong while fetching user"
-        })
-    }
-    
-}
-exports.savechat=async(req,res)=>{
-    console.log("req body",req.body)
-    var chatObj={
-        senderId:req.body.senderId,
-        receiverId:req.body.receiverId,
-        message:req.body.message
-    }
-    try{
-        const chat= await Chat.create(chatObj)
-        // console.log(chat)
-        res.status(201).send({
-            success:true,
-            msg:"Chat created successfully",
-        data:chat})
-    }
-    catch(error){
-        console.log(error.message)
-        res.status(400).send({success:false,msg:error.message})
-    }
-}
+const User = require("../models/auth.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const key = require("../config/secretkey.config");
+const Chat = require("../models/chat.model");
+exports.signup = async (req, res) => {
+	console.log("req body", req.body);
+	const userObj = {
+		name: req.body.name,
+		email: req.body.email,
+		phone: req.body.phone,
+		password: bcrypt.hashSync(req.body.password, 8),
+		country: req.body.country,
+		image: req.file.filename,
+	};
+	try {
+		const user = await User.create(userObj);
+		res.render("pages-login");
+	} catch (error) {
+		console.log(error.message);
+		res
+			.status(404)
+			.send({ msg: "Something went wrong while creating new user" });
+	}
+};
+exports.signin = async (req, res) => {
+	try {
+		let user = await User.findOne({ email: req.body.email });
+		let otheruser = await User.find({ email: { $nin: [user.email] } });
+		if (!user) {
+			return res.status(201).send({
+				msg: "User does not exist",
+			});
+		}
+		const isValidPassword = bcrypt.compareSync(
+			req.body.password,
+			user.password
+		);
+		if (!isValidPassword) {
+			return res.status(201).send({ msg: "Password does not match" });
+		}
+		const token = jwt.sign({ _id: user }, process.env.SECRET_KEY);
+		const response = {
+			name: req.body.name,
+			email: req.body.email,
+			phone: req.body.phone,
+			location: req.body.location,
+			token: token,
+		};
+		res.render("chat", {
+			message: "user created successfully",
+			user: user,
+			other: otheruser,
+		});
+	} catch (error) {
+		console.log(error.message);
+		res.status(404).send({
+			msg: "Something went wrong while fetching user",
+		});
+	}
+};
+exports.savechat = async (req, res) => {
+	// console.log("req body", req.body);
+	var chatObj = {
+		senderId: req.body.senderId,
+		receiverId: req.body.receiverId,
+		message: req.body.message,
+	};
+	try {
+		const chat = await Chat.create(chatObj);
+		// console.log(chat)
+		res.status(201).send({
+			success: true,
+			msg: "Chat created successfully",
+			data: chat,
+		});
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ success: false, msg: error.message });
+	}
+};
--- server.js
+++ server.js
@@ -1,63 +1,77 @@
-const express=require('express')
-const bodyparser=require('body-parser')
-const mongoose=require('mongoose')
-const ejs=require('ejs')
-const User=require('./models/auth.model')
-const app=express()
-const http = require('http');
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const ejs = require("ejs");
+const User = require("./models/auth.model");
+const app = express();
+const http = require("http");
 const server = http.createServer(app);
-const io=require('socket.io')(server)
-const dbConfig=require('./config/db.config')
-const serverConfig=require('./config/server.config')
-
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(express.static(__dirname + '/public'));
+const io = require("socket.io")(server);
+const dbConfig = require("./config/db.config");
+const serverConfig = require("./config/server.config");
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+	})
+);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 // var socketConfig=io.of('/user-namespace')
-io.on('connection',async (socket)=>{
-    console.log("User Connected")
-    const loginuserId=socket.handshake.auth.token
-    await User.findByIdAndUpdate({_id:loginuserId},{$set:{isOnline:true}})
-    // console.log(socket.handshake.auth.token)
-    // socket.on('login', function(data){
-    //     console.log('a user ' + data.userId + ' connected');
-    // });
-    socket.on('disconnect',async()=>{
-        console.log("User disconnected")
-        await User.findByIdAndUpdate({_id:loginuserId},{$set:{isOnline:false}})
-    })
-    //chatting implementation
-    socket.on('newChat',function(data){
-        socket.broadcast.emit('loadnewchat',data)
-    })
-})
+io.on("connection", async (socket) => {
+	console.log("User Connected");
+	const loginuserId = socket.handshake.auth.token;
+	await User.findByIdAndUpdate(
+		{ _id: loginuserId },
+		{ $set: { isOnline: true } }
+	);
+	// console.log(socket.handshake.auth.token)
+	// socket.on('login', function(data){
+	//     console.log('a user ' + data.userId + ' connected');
+	// });
+	socket.on("disconnect", async () => {
+		console.log("User disconnected");
+		await User.findByIdAndUpdate(
+			{ _id: loginuserId },
+			{ $set: { isOnline: false } }
+		);
+	});
+	//chatting implementation
+	socket.on("newChat", function (data) {
+		socket.broadcast.emit("loadnewchat", data);
+	});
+});
 //database setup
-mongoose.connect(dbConfig.DB_URL).then(()=>{
-    console.log("database connected successfully")
-}).catch((error)=>{
-    console.log(error.message)
-})
+mongoose
+	.connect(dbConfig.DB_URL)
+	.then(() => {
+		console.log("database connected successfully");
+	})
+	.catch((error) => {
+		console.log(error.message);
+	});
 
 //setup view engine
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
 //showing login page
 app.get("/login", function (req, res) {
-    res.render("pages-login");
+	res.render("pages-login");
 });
 
 //showing register page
-app.get('/',(req,res)=>{
-    res.render('pages-register')
-})
+app.get("/", (req, res) => {
+	res.render("pages-register");
+});
 
 //display error page
-app.get('/*',(req,res)=>{
-    res.render('pages-404')
-})
+app.get("/*", (req, res) => {
+	res.render("pages-404");
+});
 
-require('./routes/auth.routes')(app)
+require("./routes/auth.routes")(app);
 
-server.listen(serverConfig.PORT,()=>{
-    console.log("Server is listen on port",serverConfig.PORT)
-})
+server.listen(serverConfig.PORT, () => {
+	console.log("Server is listen on port", serverConfig.PORT);
+});
